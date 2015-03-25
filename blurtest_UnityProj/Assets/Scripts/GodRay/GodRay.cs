97318c14d3c83e66866a3343b1d1be298f76a9e1
yuya <yuya.ono@monstar-lab.com>
2015-03-25 12:13:21 +0900

    Start point of GodRay

---
+++ blurtest_UnityProj/Assets/Scripts/GodRay/GodRay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
